0e27770258343faaa224b084a31f9bb37a230379
Khushi Agrawal <khushi.ag612@gmail.com>
2025-08-07 15:48:58 +0530

    add Subtraction.java with subtraction logic

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int x=subtract(5,2);
+        System.out.println(x);
+
+    }
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+}
